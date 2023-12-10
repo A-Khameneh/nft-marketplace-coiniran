da783fcaf245d1d5860b4f5ba5ea273330e4908d
Davoud Abdollahian <mt.d.abdollahian@gmail.com>
2023-12-10 14:15:36 +0330

    market contract is created

--- contracts/market.sol
+++ contracts/market.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 import "hardhat/console.sol";
 
-contract Marketplace is ReentrancyGuard {
+contract Market {
+
+    // Variables
+    address payable public immutable feeAccount; // the account that receives fees
+    uint public immutable feePercent; // the fee percentage on sales 
+    uint public itemCount; 
+
+    constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+
+}
