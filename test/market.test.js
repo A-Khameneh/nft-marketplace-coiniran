4f68f1f89b12c448765e200c716e0504086dd238
Davoud Abdollahian <mt.d.abdollahian@gmail.com>
2023-12-11 13:11:22 +0330

    'Making marketplace items' test is done!

--- test/market.test.js
+++ test/market.test.js
@@ -1,4 +1,7 @@
 const { expect } = require("chai"); 
+const { ethers } = require("hardhat");
+
+const toWei = (num) => ethers.parseEther(num.toString())
 
 describe("NFTMarketplace", () => {
 
@@ -58,5 +61,48 @@ describe("NFTMarketplace", () => {
     });
     
   })
+
+  describe("Making marketplace items", function () {
+    let price = 1
+    let result 
+    beforeEach(async function () {
+      // addr1 mints an nft
+      await nft.connect(addr1).mint(URI)
+      // addr1 approves marketplace to spend nft
+      await nft.connect(addr1).setApprovalForAll(await market.getAddress(), true)
+    })
+
+
+    it("Should track created item, transfer NFT from seller to marketplace and emit Offered event", async function () {
+      // addr1 offers their nft at a price of 1 ether
+      await expect(market.connect(addr1).makeItem(await nft.getAddress(), 1 , toWei(price)))
+        .to.emit(market, "Offered")
+        .withArgs(
+          1,
+          await nft.getAddress(),
+          1,
+          toWei(price),
+          addr1.address
+        )
+      // Owner of NFT should now be the marketplace
+      expect(await nft.ownerOf(1)).to.equal(await market.getAddress());
+      // Item count should now equal 1
+      expect(await market.itemCount()).to.equal(1)
+      // Get item from items mapping then check fields to ensure they are correct
+      const item = await market.items(1)
+      expect(item.itemId).to.equal(1)
+      expect(item.nft).to.equal(await nft.getAddress())
+      expect(item.tokenId).to.equal(1)
+      expect(item.price).to.equal(toWei(price))
+      expect(item.sold).to.equal(false)
+    });
+
+    it("Should fail if price is set to zero", async function () {
+      await expect(
+        market.connect(addr1).makeItem(await nft.getAddress(), 1, 0)
+      ).to.be.revertedWith("Price must be greater than zero");
+    });
+
+  });
 
 })
