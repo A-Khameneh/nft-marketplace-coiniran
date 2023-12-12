96e1e4c2bb85c18613ee9fef2c388b931265dfbf
Davoud Abdollahian <mt.d.abdollahian@gmail.com>
2023-12-12 13:12:22 +0330

    add purchaseItem function

--- contracts/market.sol
+++ contracts/market.sol
@@ -32,6 +32,15 @@ contract Market {
         address indexed seller
     );
 
+    event Bought(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller,
+        address indexed buyer
+    );
+
     constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
@@ -66,6 +75,36 @@ contract Market {
             _price,
             msg.sender
         );
+    }
+
+    function purchaseItem(uint _itemId) external payable {
+
+        uint _totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+        
+        require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
+        require(msg.value >= _totalPrice, "not enough ether to cover item price and market fee");
+        require(!item.sold, "item already sold");
+        // pay seller and feeAccount
+        item.seller.transfer(item.price);
+        feeAccount.transfer(_totalPrice - item.price);
+        // update item to sold
+        item.sold = true;
+        // transfer nft to buyer
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+        // emit Bought event
+        emit Bought(
+            _itemId,
+            address(item.nft),
+            item.tokenId,
+            item.price,
+            item.seller,
+            msg.sender
+        );
+    }
+
+    function getTotalPrice(uint _itemId) view public returns(uint){
+        return((items[_itemId].price*(100 + feePercent))/100);
     }
 
 }
