7492058a56b0d68073429b3eb46910352e0b8421
Davoud Abdollahian <mt.d.abdollahian@gmail.com>
2023-12-22 15:42:59 +0330

    contracts published on sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL 
+const PRIVATE_KEY = process.env.PRIVATE_KEY 
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
+
+  defaultNetwork: "hardhat",
+  networks: {
+      hardhat: {
+          chainId: 1337,
+          // gasPrice: 130000000000,
+      },
+      sepolia: {
+          url: SEPOLIA_RPC_URL,
+          accounts: [PRIVATE_KEY],
+          chainId: 11155111,
+          blockConfirmations: 6,
+      },
+  },
+
   solidity: "0.8.20",
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+  },
 };
